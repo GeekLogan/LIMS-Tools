00e4f91e587396f39a18d0419e3d4f5a9a571eae
Logan Walker <walker.1698@osu.edu>
2016-04-12 14:52:15 -0400

    Update sample config

--- sample_config/config.php
+++ sample_config/config.php
@@ -1,11 +1,11 @@
 <?php
 //THIS IS THE CONFIGURATION FILE
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $password = "1234";
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "";
 
 session_start();
 
@@ -19,6 +19,25 @@ function check_log_inverse() {
 	if( $_SESSION["logid"] != null ) {
 		header( 'Location: index.php' );
 	}
+}
+
+function connect_DB() {
+	return new mysqli("localhost", "root", "", "mendeLIMS_dev");
+}
+
+function log_HTML($txt){
+	echo "<!-- ";
+	echo $txt;
+	echo " --!>";
+}
+
+function qdb($query) {
+	log_HTML("DB Query: ".$query);
+	return connect_DB()->query($query);
+}
+
+function qdb_f($query) {
+	return qdb($query)->fetch_assoc();
 }
 
 ?>
